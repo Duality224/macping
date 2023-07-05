528e48499d45bcc732c596a644edd5c61900c1e8
Duality224 <98560612+Duality224@users.noreply.github.com>
2023-07-05 16:47:19 +1200

    Update macping.sh

--- macping.sh
+++ macping.sh
@@ -8,5 +8,9 @@ fi
 
 #resetting arp cache
 sudo ip link set arp off dev [NETWORK INTERFACE, e.g. wlo1] && sudo ip link set arp on dev [NETWORK INTERFACE, e.g. wlo1]
-#running arp-scan on local network, grepping input argument in arp-scan output (should be MAC address), outputting corresponding IP address, and pinging corresponding IP address
-ping -c 5 -w 1000 $(sudo arp-scan --retry=5 --interface=[NETWORK INTERFACE, e.g. wlo1] [NETWORK ADDRESS RANGE, e.g. 192.168.1.0/24] | grep -m 1 $1 | awk ' { printf $1 } ')
+#running arp-scan on local network, grepping input argument in arp-scan output (should be MAC address), and outputting corresponding IP address
+IP=$(sudo arp-scan --retry=5 --interface=[NETWORK INTERFACE, e.g. wlo1] [NETWORK ADDRESS RANGE, e.g. 192.168.1.0/24] | grep -m 1 $1 | awk ' { printf $1 } ')
+#running nslookup on outputted IP address
+nslookup "$IP"
+#pinging outputted IP address
+ping -c 5 -w 1000 "$IP"
